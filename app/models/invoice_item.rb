437de655cdea5e62447d2eb500bbf21cccaa5186
Tylor Schafer <tylorschafer@gmail.com>
2019-10-01 17:16:25 -0600

    Add customer name validations and invoice_item validation and relationship testing

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,6 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  validates_numericality_of :quantity, :unit_price
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  describe 'Validations' do
+    it {should validate_numericality_of :quantity}
+    it {should validate_numericality_of :unit_price}
+  end
+
+  describe 'Relationships' do
+    it {should belong_to :item}
+    it {should belong_to :invoice}
+  end
 end
