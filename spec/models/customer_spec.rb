7ff091d2dba59a3f82cfd28e265aefb125f32cc7
Tylor Schafer <tylorschafer@gmail.com>
2019-10-01 17:12:34 -0600

    Add customer validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'Validations' do
+    it {should validate_presence_of :first_name}
+    it {should validate_presence_of :last_name}
   end
 
   describe 'Relationships' do
