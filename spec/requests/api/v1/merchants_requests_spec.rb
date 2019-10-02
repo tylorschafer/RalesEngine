5d8f4d54018cb10359ba30fc29cb79cdbb252579
Tylor Schafer <tylorschafer@gmail.com>
2019-10-01 20:25:30 -0600

    Add merchant by most_revenue endpoint test

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
-# describe 'Merchants API' do
-#   it "Returns the top merchants ranked by revenue" do
-#     bob = create(:merchant)
-#     rob = create(:merchant)
-#     cob = create(:merchant)
-#     hobb = create(:merchant)
-#     knob = create(:merchant)
-#
-#     get '/api/v1/merchants/most_revenue?quantity=3'
-#
-#
-#   end
-# end
+describe 'Merchants API' do
+  it "Returns the top merchants ranked by revenue" do
+    bob = create(:merchant)
+    allow(bob).to receive(:total_revenue).and_return(10000)
+    rob = create(:merchant)
+    allow(rob).to receive(:total_revenue).and_return(1000)
+    cob = create(:merchant)
+    allow(cob).to receive(:total_revenue).and_return(2000)
+    hobb = create(:merchant)
+    allow(hobb).to receive(:total_revenue).and_return(3000)
+    knob = create(:merchant)
+    allow(knob).to receive(:total_revenue).and_return(4000)
+
+    get '/api/v1/merchants/most_revenue?quantity=3'
+
+    result = JSON.parse(response.body)
+
+    expect(result).to be_successful
+    expect(result["data"].count).to eq(3)
+    expect(result["data"]).to have_content(bob.name)
+    expect(result["data"]).to have_content(knob.name)
+    expect(result["data"]).to have_content(hobb.name)
+  end
+end
