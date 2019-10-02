3ee78cee56317e88e6480010b0937381b5259362
Tylor Schafer <tylorschafer@gmail.com>
2019-10-02 10:16:14 -0600

    Add addtional customer and transaction relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,5 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
 
   has_many :invoices
+  has_many :invoice_items, through: :invoices
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,7 @@
 class Transaction < ApplicationRecord
   validates_numericality_of :credit_card_number
   validates_presence_of :credit_card_expiration_date, :result
+
   belongs_to :invoice
+  has_many :invoice_items, through: :invoice
 end
