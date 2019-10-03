2ff9852cf46e83a7132cc23217935fa3beec381d
Tylor Schafer <tylorschafer@gmail.com>
2019-10-03 10:36:33 -0600

    Add customer find_all_by model test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Customer, type: :model do
   describe 'Relationships' do
     it {should have_many :invoices}
   end
+
+  describe 'Class Methods' do
+    it "find_all_by" do
+      tylor_1 = create(:customer, first_name: 'Tylor')
+      tylor_2 = create(:customer, first_name: 'Tylor', last_name: 'Schafer')
+      schafer_2 = create(:customer, last_name: 'Schafer')
+
+      expect(Customer.find_all_by(first_name: 'Tylor')).to eq([tylor_1, tylor_2])
+      expect(Customer.find_all_by(last_name: 'Schafer')).to eq([tylor_2, schafer_2])
+    end
+  end
 end
