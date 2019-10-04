421247976d8c8598a202dcf19051365a84334aaf
Tylor Schafer <tylorschafer@gmail.com>
2019-10-04 17:58:50 -0600

    Add all passing item endpoints

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::FindController < Api::V1::Items::BaseController
 
   def show
-    if find_params[:unit_price] != nil
+    if find_params[:unit_price]
       render json: ItemSerializer.new(Item.find_by_price(find_params[:unit_price]))
     else
       render json: ItemSerializer.new(Item.find_by(find_params))
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,10 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
 
+  def self.find_by(params)
+    Item.order(:id).find_by(params)
+  end
+
   def self.find_all_by(params)
     Item.where(params).order(:id)
   end
