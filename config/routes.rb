391783e0a3da2f6f3736d61abc8a8db40b070737
Tylor Schafer <tylorschafer@gmail.com>
2019-10-05 14:58:55 -0600

    Add merchant invoices request spec

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
         get 'find', to: 'find#show'
         get 'find_all', to: 'find_all#index'
         get ':id/items', to: 'items#index'
+        get ':id/invoices', to: 'invoices#index'
       end
 
       resources :merchants, only:[:index, :show]
